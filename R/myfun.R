5136926ebb5c00aa0aa548f73cb12b7e54d96efb
Treywizard <trey.lavender@yahoo.com>
2022-07-13 12:18:21 -0500

    Initial commit

---
+++ R/myfun.R
@@ -0,0 +1,11 @@
+#' @title Square a vector
+#'
+#' @param x A vector of quantitative values
+#'
+#' @return A vector of squared values
+#' @export
+#'
+#' @examples myfun(x = 1:4)
+myfun <- function(x){
+  x^2
+}
